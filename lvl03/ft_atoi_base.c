e7683ab57873d647883771753a82b56c039f8c5b
Zineb Ait-Errami <zait-err@c4r1p7.1337.ma>
2025-02-19 14:23:42 +0100

    LVL03

---
+++ lvl03/ft_atoi_base.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int	ft_atoi_base(const char *str, int str_base)
+{
+    int i;
+    int nb;
+    int sign;
+
+    i = 0;
+    nb = 0;
+    sign = 1;
+
+    while(str[i] == 32 || str[i] >= 9 && str[i] <= 13)
+        i++;
+    if(str[i] == '-' || str[i] == '+')
+    {
+        if(str[i] == '-')
+            sign = -1;
+        i++;
+    }
+    while(str[i])
+    {
+        nb = nb * str_base;
+        if(str[i] >= 'a' && str[i] <= 'z')
+            nb += str[i] - 87;
+        else if(str[i] >= 'A' && str[i] <= 'Z')
+            nb += str[i] - 55;
+        else if(str[i] >= '0' && str[i] <= '9')
+            nb += str[i] - 48;
+        i++;
+    }
+    return (nb * sign);
+}

c7f0b83572d19f380201fc6014b9d6bcf25a27ab
Zineb Ait-Errami <zait-err@c4r3p2.1337.ma>
2025-02-25 18:24:30 +0000

    ex

--- lvl04/ft_list_remove_if.c
+++ lvl04/ft_list_remove_if.c
@@ -8,11 +8,20 @@ typedef struct      s_list
 
 void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
+
+    if(begin_list == NULL || *begin_list == NULL)
+        return ;
+
     t_list *lst;
 
     lst = *begin_list;
-    while(lst)
+
+    if(cmp(lst->data, data_ref) == 0)
     {
-        (*)
+        *begin_list = lst->next;
+        free(lst);
+        ft_list_remove_if(begin_list, data_ref, cmp);
     }
+    lst = *begin_list;
+    ft_list_remove_if(&lst->next, data_ref, cmp);
 }

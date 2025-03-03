a52359664dc96b6da8ceff3eba2249b8a4accc18
Zineb Ait-Errami <zait-err@c4r1p3.1337.ma>
2025-03-03 23:40:33 +0000

    update last word

--- lvl01/last_word.c
+++ lvl01/last_word.c
@@ -15,21 +15,19 @@ int main(int ac, char **av)
         while (av[1][i] != '\0')
             i++;
         len = i - 1;
-        printf("%d\n", av[1][len]);
         while (av[1][len] == ' ' || av[1][len] == '\t')
         {
             len--;
-            printf("test\n");
         }
         while (av[1][len] != ' ' && av[1][len] != '\t' && len > 0)
         {   
-            printf("len: %d\n", len);
             len--;
         }
         i = len;
         while (av[1][i])
         {
-            write(1, &av[1][i], 1);
+            if (av[1][i] != ' ' && av[1][i] != '\t')
+                write(1, &av[1][i], 1);
             i++;
         }
     }
